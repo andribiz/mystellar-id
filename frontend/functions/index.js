300a3ac7c86c3b3e04cc951f9dc3e3e0a623f083
Andrix <>
2019-09-20 19:42:44 +0700

    Restructuring for firebase publish

---
+++ frontend/functions/index.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const functions = require('firebase-functions');
+const next = require('next');
+
+let dev = process.env.NODE_ENV !== 'production';
+let app = next({
+  dev,
+  conf: { distDir: "next" },
+});
+let handle = app.getRequestHandler();
+
+exports.next = functions.https.onRequest((req, res) => {
+  console.log('File: ' + req.originalUrl); // log the page.js file that is being requested
+  return app.prepare().then(() => handle(req, res));
+});
