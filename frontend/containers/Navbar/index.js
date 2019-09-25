980da48c15760bc2fae8da2eca6f97819a526363
dinafitria <dinafitria@bizoftenterprise.com>
2019-09-25 14:08:19 +0700

    revise nav

--- frontend/containers/Navbar/index.js
+++ frontend/containers/Navbar/index.js
@@ -10,6 +10,7 @@ import Logo from '../../elements/UI/Logo';
 import Button from '../../elements/Button';
 import Container from '../../components/UI/Container';
 import LoginModal from '../LoginModal';
+import Scrollspy from 'react-scrollspy';
 
 import NavbarWrapper, { MenuArea, MobileMenu } from './navbar.style';
 
@@ -41,40 +42,54 @@ const Navbar = () => {
 
   // Authentication modal handler
   const handleLoginModal = () => {
-    openModal({
-      config: {
-        className: 'login-modal',
-        disableDragging: true,
-        width: '100%',
-        height: '100%',
-        animationFrom: { transform: 'translateY(100px)' }, // react-spring <Spring from={}> props value
-        animationTo: { transform: 'translateY(0)' }, //  react-spring <Spring to={}> props value
-        transition: {
-          mass: 1,
-          tension: 180,
-          friction: 26,
-        },
-      },
-      component: LoginModal,
-      componentProps: {},
-      closeComponent: CloseModalButton,
-      closeOnClickOutside: false,
-    });
+    // openModal({
+    //   config: {
+    //     className: 'login-modal',
+    //     disableDragging: true,
+    //     width: '100%',
+    //     height: '100%',
+    //     animationFrom: { transform: 'translateY(100px)' }, // react-spring <Spring from={}> props value
+    //     animationTo: { transform: 'translateY(0)' }, //  react-spring <Spring to={}> props value
+    //     transition: {
+    //       mass: 1,
+    //       tension: 180,
+    //       friction: 26,
+    //     },
+    //   },
+    //   component: LoginModal,
+    //   componentProps: {},
+    //   closeComponent: CloseModalButton,
+    //   closeOnClickOutside: false,
+    // });
+
+    window.location.href = '/login';
   };
 
   return (
     <NavbarWrapper className="navbar">
       <Container>
-        <Logo
-          className="logo"
-          href="/appclassic"
-          logoSrc={logo}
-          title="App Classic"
-        />
+        <Logo className="logo" href="/" logoSrc={logo} title="MyStellar ID" />
         {/* end of logo */}
 
         <MenuArea>
-          <ScrollSpyMenu className="menu" menuItems={navMenu} offset={-84} />
+          {/*<ScrollSpyMenu className="menu" menuItems={navMenu} offset={-84} />*/}
+
+          {/*<nav>*/}
+          <Scrollspy className="menu" items={['1', '2', '3']} offset={-84}>
+            <li>
+              <a href="/#tools">Tools</a>
+            </li>
+            <li>
+              <a href="/#faq">FAQ</a>
+            </li>
+            <li>
+              <a href="/#home">
+                <Button className="trail" title="Try for Free" />
+              </a>
+            </li>
+          </Scrollspy>
+          {/*</nav>*/}
+
           {/* end of main menu */}
 
           <Button
@@ -85,12 +100,15 @@ const Navbar = () => {
             aria-label="login"
           />
 
-          <AnchorLink href="#home" offset={84}>
-            <Button className="trail" title="Try for Free" />
-          </AnchorLink>
+          {/*<AnchorLink href="#home" offset={84}>*/}
+          {/*  <Button className="trail" title="Try for Free" />*/}
+          {/*</AnchorLink>*/}
+
+          {/*<Button className="trail" title="Try for Free" href="http://localhost:3000/#home"/>*/}
 
           <Button
             className="menubar"
+            href="/"
             icon={
               state.mobileMenu ? (
                 <Icon className="bar" icon={x} />
---
+++ frontend/pages/login.js
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from 'react';
+import Head from 'next/head';
+import { ThemeProvider } from 'styled-components';
+import Sticky from 'react-stickynode';
+import { appTheme } from '../theme';
+import { AppWrapper, GlobalStyle } from '../containers/app.style';
+import { ResetCSS } from '../assets/css/style';
+import Navbar from '../containers/Navbar';
+// import NavbarOld from '../containers/NavbarOld';
+// import DomainSection from '../containers/Banner';
+// import APISection from '../containers/APISection';
+// import InfoSection from '../containers/InfoSection';
+// import Footer from '../containers/Footer';
+import { DrawerProvider } from '../contexts/DrawerContext';
+// import Faq from "../containers/Faq";
+import LoginModal from '../containers/LoginModal';
+import Footer from '../containers/Footer';
+
+function getSize() {
+  return {
+    innerHeight: window.innerHeight,
+    innerWidth: window.innerWidth,
+    outerHeight: window.outerHeight,
+    outerWidth: window.outerWidth,
+  };
+}
+
+function useWindowSize() {
+  let [windowSize, setWindowSize] = useState(getSize());
+
+  function handleResize() {
+    setWindowSize(getSize());
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return windowSize;
+}
+
+export default function Index() {
+  const size = process.browser && useWindowSize();
+  return (
+    <ThemeProvider theme={appTheme}>
+      <>
+        <Head>
+          <title>Stellar Federation Service</title>
+          <meta
+            name="Description"
+            content="stellar federation address for domain mystellar.id"
+          />
+          <meta name="theme-color" content="#ec5555" />
+          <link
+            href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700"
+            rel="stylesheet"
+          />
+        </Head>
+
+        <ResetCSS />
+        <GlobalStyle />
+
+        <AppWrapper>
+          <Sticky top={0} innerZ={9999} activeClass="sticky-nav-active">
+            <DrawerProvider>
+              <Navbar />
+            </DrawerProvider>
+          </Sticky>
+          {/*<DomainSection/>*/}
+          {/*<InfoSection />*/}
+          {/*<APISection />*/}
+          {/*<Faq/>*/}
+          {/*<Footer />*/}
+          <LoginModal />
+          <Footer />
+        </AppWrapper>
+      </>
+    </ThemeProvider>
+  );
+}
